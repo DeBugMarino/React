e7c14adc6c5da5a03a0c4f78b4cb4948925522da
DeBugMarino <159265842+DeBugMarino@users.noreply.github.com>
2025-03-11 19:34:20 +0100

    props

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import Welcome from "./welcome";
+
+function App() {
+  return <Welcome name="Davide" age="27" />;
+}
+
+export default App;
---
+++ src/welcome.jsx
@@ -0,0 +1,12 @@
+export default function Welcome({ name, age }) {
+  return (
+    <>
+      <p>
+        Welcome, <b>{name}</b>!
+      </p>
+      <p>
+        Your age is <b>{age}</b>
+      </p>
+    </>
+  );
+}
